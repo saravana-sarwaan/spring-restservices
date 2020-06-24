cc27f93fef5110ece48efaffbc192873837949e0
saravana <saravana.sarwaan@gmail.com>
2020-06-24 20:28:34 +0530

    First Commit

---
+++ src/main/java/com/saravana/restservices/spring/SpringRestServicesApplication.java
@@ -0,0 +1,13 @@
+package com.saravana.restservices.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saravana/restservices/spring/SpringRestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saravana.restservices.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
