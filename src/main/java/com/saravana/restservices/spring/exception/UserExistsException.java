ccf6012aa547ec091767606886da2c6b4f1280f1
saravana <saravana.sarwaan@gmail.com>
2020-06-29 21:12:45 +0530

    all files

---
+++ src/main/java/com/saravana/restservices/spring/exception/UserExistsException.java
@@ -0,0 +1,17 @@
+package com.saravana.restservices.spring.exception;
+
+public class UserExistsException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UserExistsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
