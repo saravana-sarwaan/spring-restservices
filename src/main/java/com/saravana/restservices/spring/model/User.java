58eb339bebda5557524b88004b48eeb0326efc60
saravana <saravana.sarwaan@gmail.com>
2020-07-20 19:50:54 +0530

    adding validations dependency

--- src/main/java/com/saravana/restservices/spring/model/User.java
+++ src/main/java/com/saravana/restservices/spring/model/User.java
@@ -15,35 +15,34 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name = "user")
 public class User {
-	
+
 	@Id
 	@GeneratedValue
 	private Long id;
-	
+
 	@NotEmpty(message = "Username cannot be empty")
-	@Size(min = 2,max = 10, message = "Username should be min of 2 characters and max of 10 characters")
-	@Column(name = "USER_NAME" , length = 50 , nullable = false, unique = true)
+	@Size(min = 2, max = 10, message = "Username should be min of 2 characters and max of 10 characters")
+	@Column(name = "USER_NAME", length = 50, nullable = false, unique = true)
 	private String userName;
-	
-	
-	@Column(name = "FIRST_NAME" , length = 50 , nullable = false)
+
+	@Column(name = "FIRST_NAME", length = 50, nullable = false)
 	private String firstName;
-	@Column(name = "LAST_NAME" , length = 50 , nullable = false)
+	@Column(name = "LAST_NAME", length = 50, nullable = false)
 	private String lastName;
-	@Column(name = "EMAIL_ADDRESS" , length = 50 , nullable = false, unique = true)
+	@Column(name = "EMAIL_ADDRESS", length = 50, nullable = false, unique = true)
 	private String email;
-	@Column(name = "ROLE" , length = 50 , nullable = false)
+	@Column(name = "ROLE", length = 50, nullable = false)
 	private String role;
-	@Column(name = "SSN" , length = 50 , nullable = false, unique = true)
+	@Column(name = "SSN", length = 50, nullable = false, unique = true)
 	private String ssn;
-	
+
 	@OneToMany(mappedBy = "user")
 	private List<Order> orders;
-	
+
 	public User() {
-		
+
 	}
-	
+
 	public User(Long id, String userName, String firstName, String lastName, String email, String role, String ssn) {
 		this.id = id;
 		this.userName = userName;
@@ -53,45 +52,59 @@ public class User {
 		this.role = role;
 		this.ssn = ssn;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getRole() {
 		return role;
 	}
+
 	public void setRole(String role) {
 		this.role = role;
 	}
+
 	public String getSsn() {
 		return ssn;
 	}
+
 	public void setSsn(String ssn) {
 		this.ssn = ssn;
 	}
@@ -108,6 +121,5 @@ public class User {
 	public String toString() {
 		return super.toString();
 	}
-	
-	
+
 }
