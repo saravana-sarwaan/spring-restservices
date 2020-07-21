cca494a3ddf15f3c28cd48925d868cbebc59c167
saravana <saravana.sarwaan@gmail.com>
2020-07-22 01:20:55 +0530

    committing internationalization

--- src/main/java/com/saravana/restservices/spring/SpringRestServicesApplication.java
+++ src/main/java/com/saravana/restservices/spring/SpringRestServicesApplication.java
@@ -1,7 +1,13 @@
 package com.saravana.restservices.spring;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 //test
 @SpringBootApplication
@@ -9,6 +15,20 @@ public class SpringRestServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestServicesApplication.class, args);
-	}	
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("message");
+		return messageSource;
+	}
 
 }
--- src/main/java/com/saravana/restservices/spring/helloController/HelloController.java
+++ src/main/java/com/saravana/restservices/spring/helloController/HelloController.java
@@ -1,7 +1,12 @@
 package com.saravana.restservices.spring.helloController;
 
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +16,9 @@ import com.saravana.restservices.spring.model.User;
 //RestController for Hello world
 @RestController
 public class HelloController {
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 	
 	
 	//Rest end point
@@ -24,6 +32,11 @@ public class HelloController {
 	@GetMapping("/userData")
 	public User getData() {
 		return new User(501L,"sarwaan007", "saravanan", "thangasamy", "saravana.thangasamy@gmail.com", "admin", "123adf");
+	}
+	
+	@GetMapping("/Hello-int")
+	public String getMessage(@RequestHeader(name = "Accept-Language", required = false) String locale) {
+		return messageSource.getMessage("label.hello",null, new Locale(locale));
 	}
 
 }
